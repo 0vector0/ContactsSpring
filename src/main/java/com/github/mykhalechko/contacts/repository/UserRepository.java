5fb4ec3058ccac1b149f38edd9a714ef1076f54f
0vector0 <0vector0@gmail.com>
2017-03-07 23:44:59 +0200

    security config

---
+++ src/main/java/com/github/mykhalechko/contacts/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.github.mykhalechko.contacts.repository;
+
+import com.github.mykhalechko.contacts.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+}
