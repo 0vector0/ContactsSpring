f46c85f3640498fbfffe9d6b57d37277a75d2163
0vector0 <0vector0@gmail.com>
2017-03-09 00:10:40 +0200

    validate contact

--- src/main/java/com/github/mykhalechko/contacts/entity/Contact.java
+++ src/main/java/com/github/mykhalechko/contacts/entity/Contact.java
@@ -16,45 +16,52 @@ public class Contact implements Serializable {
     @Column(name = "contact_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
     @Column(name = "surname")
     @NotEmpty(message = "{contact.error.surnameRequired}")
     @Size(min = 4, message = "{contact.error.surnameLength}")
     private String surname;
-
     @Column(name = "name")
     @NotEmpty(message = "{contact.error.nameRequired}")
     @Size(min = 4, message = "{contact.error.nameLength}")
     private String name;
-
     @Column(name = "patronymic")
     @NotEmpty(message = "{contact.error.patronymicRequired}")
     @Size(min = 4, message = "{contact.error.patronymicLength}")
     private String patronymic;
 
-    // TODO: 07.03.2017 check phone
     @Column(name = "mobilePhone")
     @NotEmpty(message = "{contact.error.mobilePhoneRequired}")
     @Pattern(regexp = "[0-9]+", message = "{contact.error.mobilePhoneRegexp}")
     @Size(min = 12, max = 12, message = "{contact.error.mobilePhoneLength}")
     private String mobilePhone;
 
-    // TODO: 07.03.2017 check phone
     @Column(name = "homePhone")
     @Pattern(regexp = "[0-9]+", message = "{contact.error.homePhoneRegexp}")
     @Size(min = 12, max = 12, message = "{contact.error.homePhoneLength}")
     private String homePhone;
-
     @Column(name = "address")
     private String address;
-
     @Column(name = "email")
     @Email(message = "{contact.error.email}")
     private String email;
-
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", mobilePhone='" + mobilePhone + '\'' +
+                ", homePhone='" + homePhone + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", user=" + user +
+                '}';
+    }
 
     public long getId() {
         return id;
@@ -128,18 +135,4 @@ public class Contact implements Serializable {
         this.user = user;
     }
 
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "id=" + id +
-                ", surname='" + surname + '\'' +
-                ", name='" + name + '\'' +
-                ", patronymic='" + patronymic + '\'' +
-                ", mobilePhone='" + mobilePhone + '\'' +
-                ", homePhone='" + homePhone + '\'' +
-                ", address='" + address + '\'' +
-                ", email='" + email + '\'' +
-                ", user=" + user +
-                '}';
-    }
 }
