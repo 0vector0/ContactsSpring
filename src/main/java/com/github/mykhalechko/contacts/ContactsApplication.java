c30074a4b20151fbcace7c4b311d489164608905
0vector0 <0vector0@gmail.com>
2017-03-09 00:06:58 +0200

    TODO: 08.03.2017 Error page

--- src/main/java/com/github/mykhalechko/contacts/ContactsApplication.java
+++ src/main/java/com/github/mykhalechko/contacts/ContactsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ContactsApplication {
-
+	// TODO: 08.03.2017 Error page
 	public static void main(String[] args) {
 		SpringApplication.run(ContactsApplication.class, args);
 	}
