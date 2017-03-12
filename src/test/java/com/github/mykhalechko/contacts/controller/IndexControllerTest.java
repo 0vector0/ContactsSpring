3b7d40608ab15b47b7086911f7bf52866318558b
0vector0 <0vector0@gmail.com>
2017-03-12 21:28:29 +0200

    add index controller test

---
+++ src/test/java/com/github/mykhalechko/contacts/controller/IndexControllerTest.java
@@ -0,0 +1,42 @@
+package com.github.mykhalechko.contacts.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.View;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class IndexControllerTest {
+
+    @InjectMocks
+    private IndexController controller;
+
+    @Mock
+    private View mockView;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = standaloneSetup(controller)
+                .setSingleView(mockView)
+                .build();
+    }
+
+    @Test
+//    @WithMockUser
+    public void getIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+}
